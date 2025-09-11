c5ddc439103e1bf17466a5c3c6ef5e7fecf4e840
Cysorn <83248151+cysorn@users.noreply.github.com>
2025-09-11 06:50:06 +0200

    Clean up active_buzzer.cpp

--- 09 TFT Display/src/sound/active_buzzer/active_buzzer.cpp
+++ 09 TFT Display/src/sound/active_buzzer/active_buzzer.cpp
@@ -16,44 +16,8 @@ ActiveBuzzer::ActiveBuzzer(){
 }
 
 
-//WORKAROUND:
-//Add global increment. Increment it once here, Change the global delay
-//Let it go out and iterate loop with the delay and come back as long the hand is there
-//Once hand is not there anymore, put back the standard programm delay
 void ActiveBuzzer::playAlarm(Ultrasonic ult){
     digitalWrite(BUZZER, HIGH);
     delay(400);
     digitalWrite(BUZZER, LOW);
 }
-
-/*
-void ActiveBuzzer::playAlarm(Ultrasonic ult){
-  int size = sizeof(passive_buzzer_durations) / sizeof(int);
-  Serial.println("I'm in the function");
-  
-  // Declare and initialize tmp inside the function
-  float tmp = ult.getDistanceInCm();
-  Serial.print("tmp: ");
-  Serial.println(tmp);
-  
-  do{
-    Serial.println("I'm in while");
-    for (int note = 0; note < size; note++) {
-      Serial.println("I'm in for");
-      Serial.print("tmp:");
-      Serial.println(tmp);
-      digitalWrite(BUZZER, passive_buzzer_notes[note]);
-      delay(passive_buzzer_durations[note]);
-      digitalWrite(BUZZER, !passive_buzzer_notes[note]);
-
-      if(tmp < 10 && tmp >= 0.01){
-        digitalWrite(BUZZER, LOW);
-        break;
-      }
-      // Update tmp inside the loop for continuous reading
-      tmp = ult.getDistanceInCm();
-    }
-  }while(tmp < 10 && tmp >= 0.01);
-  Serial.println("I'm ready to exit the function.");
-}
-*/
